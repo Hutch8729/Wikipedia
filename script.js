4b68d12bcbeb241a051f1e86594b74a6b2854494
Kyle Hutchens <Hutch8729@gmail.com>
2017-04-25 23:56:40 -0400

    fiddled with some of the css. I'm not as bad at it as I was a few hours ago.

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ function searchWiki(){
 	var searchString = document.getElementById("searchTxt").value;
 	var apiurl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + encodeURI(searchString) + "&srlimit=5&callback=?";
 	var searchResultsHTML = '';
+	var goToWikiBaseURL = 'https://en.wikipedia.org/wiki/';
 	console.log(searchString);
 
 	$.getJSON(apiurl, function(data){
 		console.log(data);
 		searchResultsHTML += "<div class = 'results'>";
-		searchResultsHTML += "<strong>" + data.query.search[0].title + "</strong><br>" + data.query.search[0].snippet;
+		searchResultsHTML += "<p><strong>" + data.query.search[0].title + "</strong><br>" + data.query.search[0].snippet + "</p>";
 		searchResultsHTML += "</div>";
 		$('#searchResults').html(searchResultsHTML);
 	} )
