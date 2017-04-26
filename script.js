48712789552a01bef5510f5fc6ee40156c01b57c
Kyle Hutchens <Hutch8729@gmail.com>
2017-04-26 17:34:45 -0400

    it fucking works! do a little css fiddling.

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-var searchString = document.getElementById("searchTxt").value;
-
-
 function getRandomWiki(){
-	//window.location.replace("https://en.wikipedia.org/wiki/Special:Random");
 	window.location.href = "https://en.wikipedia.org/wiki/Special:Random";
 }
 
 function searchWiki(){
 	var searchString = document.getElementById("searchTxt").value;
-	var apiurl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + encodeURI(searchString) + "&srlimit=5&callback=?";
+	var apiurl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + encodeURI(searchString) + "&callback=?";
 	var searchResultsHTML = '';
-	var goToWikiBaseURL = 'https://en.wikipedia.org/wiki/';
-	console.log(searchString);
 
 	$.getJSON(apiurl, function(data){
-		console.log(data);
-		searchResultsHTML += "<div class = 'results' id='" + data.query.search[0].title + "'>";
-		console.log(searchResultsHTML);
-		searchResultsHTML += "<p><strong>" + data.query.search[0].title + "</strong><br>" + data.query.search[0].snippet + "</p>";
-		searchResultsHTML += "</div>";
-		$('#searchResults').html(searchResultsHTML);
-		document.getElementById(data.query.search[0].title).addEventListener('click', function(){
-			window.location.href = goToWikiBaseURL + data.query.search[0].title;
-		});
+		var arr = data.query.search;
+		var goToWikiBaseURL = 'https://en.wikipedia.org/wiki/';
+		var listArr = [];
+		for(i=0; i < arr.length; i++){
+			listArr.push(data.query.search[i].title);
+			searchResultsHTML += "<div class = 'results' id='" + i + "'>";
+			searchResultsHTML += "<p><strong>" + data.query.search[i].title + "</strong><br>" + data.query.search[i].snippet + "</p>";
+			searchResultsHTML += "</div>";
+			$('#searchResults').html(searchResultsHTML);
+		}
+		$('#searchResults').on('click', '.results', this.id, function(){
+				window.location.href = goToWikiBaseURL + encodeURI(listArr[this.id]);
+			});
 	} )
-
 }
